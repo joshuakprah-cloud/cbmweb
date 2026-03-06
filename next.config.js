65730d29ed23698bbc022f4a38b24c17947ce594
Your Name <youremail@example.com>
2026-03-06 11:18:04 +0000

    Exclude /admin from CSP to allow Sanity Studio scripts

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: '/((?!admin).*)',
         headers: [
           {
             key: 'Content-Security-Policy',

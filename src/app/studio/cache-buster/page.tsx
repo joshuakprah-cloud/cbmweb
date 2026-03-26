f862aa096c71353e3e90d6550252365198fbc456
joshuakprah-cloud <joshua.k.prah@gmail.com>
2026-03-26 20:02:20 +0000

    Restore Sanity Studio cache-buster functionality
    - Restore NextStudio component in cache-buster page
    - basePath removal should resolve routing conflicts

--- src/app/studio/cache-buster/page.tsx
+++ src/app/studio/cache-buster/page.tsx
@@ -7,12 +7,5 @@ import config from '../../../../sanity.config'
 export const dynamic = 'force-dynamic'
 
 export default function StudioPage() {
-  // Temporarily return a simple page to avoid schema errors
-  return (
-    <div style={{ padding: '20px', fontFamily: 'system-ui' }}>
-      <h1>Sanity Studio</h1>
-      <p>Studio is temporarily disabled during build.</p>
-      <p>Please access via <a href="/studio">/studio</a></p>
-    </div>
-  )
+  return <NextStudio config={config} />
 }

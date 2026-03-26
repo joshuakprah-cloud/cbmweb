864187f4cecc2359269e3bae422dc8bc4908f08f
Your Name <youremail@example.com>
2026-03-26 03:20:22 +0000

    Fix build error by removing unused deskStructure import

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,7 +4,6 @@ import { visionTool } from '@sanity/vision'
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
 import { schemaTypes } from './sanity/schema'
-import { deskStructure } from './sanity/deskStructure'
 
 export default defineConfig({
   basePath: '/studio',

6194cb4c13e5dc57500903b7bb984be6178f86c1
Developer <developer@example.com>
2026-04-21 12:15:48 +0000

    Fix prop names: sermons -> messages

--- src/app/livestream/page.tsx
+++ src/app/livestream/page.tsx
@@ -154,7 +154,7 @@ export default async function LivestreamPage() {
       {/* Past Messages Section */}
       <section className="py-12 md:py-16 bg-gray-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <PastMessages sermons={recentSermons} />
+          <PastMessages messages={recentSermons} />
         </div>
       </section>
 
